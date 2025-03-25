2a722c6830b654c5aa922437fefe3125d4dc5d94
Lonmo0208 <2675932757@qq.com>
2025-03-26 02:48:09 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 }
 
 val MINECRAFT_VERSION by extra { "1.20.1" }
-val NEOFORGE_VERSION by extra { "47.3.22" }
+val NEOFORGE_VERSION by extra { "47.4.0" }
 val FABRIC_LOADER_VERSION by extra { "0.16.10" }
-val FABRIC_API_VERSION by extra { "0.92.3+1.20.1" }
+val FABRIC_API_VERSION by extra { "0.92.5+1.20.1" }
 
 // https://semver.org/
 val MOD_VERSION by extra { "1.8.0" }
