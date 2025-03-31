a6f28101d94e753d4894ae92e3c0e4b9b3f0b056
Lonmo0208 <2675932757@qq.com>
2025-03-31 08:14:26 +0800

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val SODIUM_DEPENDENCY_NEO by extra { "maven.modrinth:sodium:I9RMZOOH"}
 val PARCHMENT_VERSION by extra { null }
 
 // https://semver.org/
-val MOD_VERSION by extra { "1.8.8" }
+val MOD_VERSION by extra { "1.8.8-Asycn" }
 
 allprojects {
     apply(plugin = "java")
@@ -55,9 +55,9 @@ subprojects {
 
         if (!isReleaseBuild) {
             if (buildId != null) {
-                builder.append("-build.${buildId}")
-            } else {
-                builder.append("-local")
+               builder.append("-build.${buildId}")
+           } else {
+                //builder.append("-local")
             }
         }
 
