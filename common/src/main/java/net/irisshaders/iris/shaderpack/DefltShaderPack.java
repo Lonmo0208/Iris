b0d20784f2e8368878f188e52e95a281926a9861
Lonmo <2675932757@qq.com>
2026-02-25 00:59:02 +0800

    Update DefltShaderPack.java

--- common/src/main/java/net/irisshaders/iris/shaderpack/DefltShaderPack.java
+++ common/src/main/java/net/irisshaders/iris/shaderpack/DefltShaderPack.java
@@ -50,18 +50,11 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
